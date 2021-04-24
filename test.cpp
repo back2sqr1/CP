e5645adb9aa5d89d31f0890da0888341d32cb9d4
David <theredhairedyonko@gmail.com>
2021-04-24 18:23:58 -0500

    Test and Template

---
+++ test.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef vector<int> vi; 
+typedef vector<pair<int,int>> vpi;
+typedef pair<int,int> pi;
+typedef long long ll; 
+#define FIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define F1R(i,a) FOR(i,1,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+void setIO(string name = "") { 
+    ios_base::sync_with_stdio(0); cin.tie(0); 
+   
+    if (sz(name)) {
+        freopen((name+".txt").c_str(), "r", stdin); 
+        freopen((name+"-23.txt").c_str(), "w", stdout);
+    }
+}
+bool cmp(const pair<int,int>&a, const pair<int,int> &b)
+{
+	if(a.f==b.f)
+	return a.s<b.s;
+	
+	return a.f<b.f;
+}
+const int SZ=1001;
+//bool box[SZ][SZ], vis[SZ][SZ];
+
+void bfs()
+{
+	
+}
+void dfs(int node)
+{
+	
+}
+void check()
+{
+	
+}
+vector<string> ans;
+void solve()
+{
+	int n; cin>>n;
+	int mx=0;
+	vi v(n); trav(x, v){cin>>x; mx=max(mx, x);}
+   		
+}
+
+int main() {
+    FIO;
+    cout<<"test"<<endl;
+	
+	
+}
+
+
